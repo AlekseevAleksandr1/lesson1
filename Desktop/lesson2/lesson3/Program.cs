d8d210bdd308628fd560da7a51244fdc29a2e08d
sasha <alekseev.aleksandr2789@gmail.com>
2023-08-10 16:03:19 +0400

    trhird commit

---
+++ Desktop/lesson2/lesson3/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] num = number(10, 10, 100);
+Console.WriteLine($"{String.Join(",", num)}");
+
+
+
+
+int[] number(int size, int minvalue, int maxvalue){
+    int[] massive = new int[size];
+    for(int i = 0; i < size; i++){
+        massive[i] = new Random().Next(minvalue, maxvalue);
+    }
+    return massive;
+}
