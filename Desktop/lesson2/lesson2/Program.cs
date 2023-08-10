47e5003fd9975e564a6ec333f34f520e5150043d
sasha <alekseev.aleksandr2789@gmail.com>
2023-08-10 15:45:43 +0400

    second commit

---
+++ Desktop/lesson2/lesson2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(sum(number));
+
+
+
+
+int sum(int number){
+    int result = 0;
+    while(number > 0){
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
