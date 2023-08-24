165b9ef43e6d92c9e2e07b23b75a8d450b771299
sasha <alekseev.aleksandr2789@gmail.com>
2023-08-24 17:02:06 +0400

    first commit

---
+++ Desktop/lesson2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите числа");
+string number = Console.ReadLine();
+Console.WriteLine(str(number));
+
+
+int str(string number){
+    int cou = 0;
+    string[] str1 = number.Split(',');
+    foreach (var num in str1){
+        if(Convert.ToInt32(num) > 0) {
+            cou = cou + 1;
+        }
+        
+    }
+    return cou;
+}
